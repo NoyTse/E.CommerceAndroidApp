fe2a8319fa732dc6b6b0a5491ed81830ced4e116
Noy Tsemach <Noy.Tse@walla.co.il>
2018-05-15 13:18:30 +0300

    hh

--- app/src/main/java/com/example/noytse/loginfacebook/ProductDetails.java
+++ app/src/main/java/com/example/noytse/loginfacebook/ProductDetails.java
@@ -1,18 +1,26 @@
 package com.example.noytse.loginfacebook;
 
+import android.app.Dialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.squareup.picasso.Picasso;
 
 import java.util.List;
 
 public class ProductDetails extends AppCompatActivity {
     Product mProduct;
+    private ListView mReviewListView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,7 +37,7 @@ public class ProductDetails extends AppCompatActivity {
         TextView lblPrice = findViewById(R.id.prodDetail_price);
         Button btnPurchase = findViewById(R.id.prodDetail_btnPurchase);
         Button btnAddReview = findViewById(R.id.prodDetail_btnAddReview);
-        ListView reviewListView = findViewById(R.id.prodDetail_reviewList);
+        mReviewListView = findViewById(R.id.prodDetail_reviewList);
 
         if (mProduct.getPhotoURL() != null)
             Picasso.with(this).load(mProduct.getPhotoURL()).into(imgProdPhoto);
@@ -42,6 +50,44 @@ public class ProductDetails extends AppCompatActivity {
         lblPrice.setText(mProduct.getPrice());
 
         //TODO set adapter to reviewListView
+        mReviewListView.setAdapter(new ReviewAdapter(mProduct.getReviewList(),this));
+
+        btnAddReview.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showReviewFormDialog();
+            }
+        });
+    }
+
+    private void showReviewFormDialog() {
+        final Dialog addReviewDialog = new Dialog(this);
+        addReviewDialog.setContentView(R.layout.review_add_form);
+        addReviewDialog.findViewById(R.id.addReview_btnSend).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText txtReviewContent = addReviewDialog.findViewById(R.id.addReview_txtContent);
+                if (txtReviewContent.getText().toString().trim().length() > 0) {
+                    addReviewToStorage(txtReviewContent.getText().toString().trim());
+                    addReviewToList(txtReviewContent.getText().toString().trim());
+                    mReviewListView.setAdapter(new ReviewAdapter(mProduct.getReviewList(),getApplicationContext()));
+                }
+            }
+        });
+    }
+
+    private void addReviewToList(String reviewContent) {
+        String userName = FirebaseAuth.getInstance().getCurrentUser().getDisplayName();
+        if (userName != null){
+            mProduct.getReviewList().add(new Review(userName,reviewContent));
+
+        }
+    }
+
+    private void addReviewToStorage(String reviewContent) {
+        //TODO
+        //here should be the code that save the review to firbase storage (dont forget to add the userID from firbaseUser)
 
     }
+
 }
